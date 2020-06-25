a44de2f0d2f60ccb0cf17c319810b408401191f8
Scott Worthington <scworthi@redhat.com>
2020-06-25 16:39:41 -0400

    added env vars

--- build/web_exec_with_podman.sh
+++ build/web_exec_with_podman.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-podman run --rm -it -p 8080:8080 localhost/ocp4-exp01-web
+podman run --rm -it \
+-e MYDATA_SOURCE_DIR='static' \
+-e MYTEMPLATE_SOURCE_DIR='templates' \
+-p 8080:8080 localhost/ocp4-exp01-web
+
