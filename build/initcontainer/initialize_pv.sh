bd7f25ca19c9d64e80ad8e2fa633868a301aa899
Scott Worthington <scworthi@redhat.com>
2020-06-26 12:46:30 -0400

    check for env vars logic

--- build/initcontainer/initialize_pv.sh
+++ build/initcontainer/initialize_pv.sh
@@ -6,7 +6,21 @@ if [[ -z "$S3BUCKET" || ! "$S3BUCKET" == "true" ]] ; then
   exit 0
 fi
 
+
 if [[ "$S3BUCKET" == "true" ]]; then
+
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+  echo "AWS_ACCESS_KEY_ID env var needs to be set, aborting."
+  exit 1
+fi
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "AWS_SECRET_ACCESS_KEY env var needs to be set, aborting."
+  exit 1
+fi
+if [[ -z "$AWS_DEFAULT_REGION" ]]; then
+  echo "AWS_DEFAULT_REGION env var needs to be set, aborting."
+  exit 1
+fi
   aws s3 sync --no-progress s3://scw-initcont-exp01/exp01-annoydata/ /usr/share/html/
   exit 0
 fi
