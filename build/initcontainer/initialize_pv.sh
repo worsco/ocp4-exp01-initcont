1e4d10b709a496031dce8d787d4f3dd33a3cb902
Scott Worthington <scworthi@redhat.com>
2020-06-26 11:13:19 -0400

    enable pulling image from S3

--- build/initcontainer/initialize_pv.sh
+++ build/initcontainer/initialize_pv.sh
@@ -1,2 +1,14 @@
-cp -f -v files/space_cat.png /usr/share/html && \
-cp -f -v files/hyttioaoa.png /usr/share/html
+# We will only sync from the S3BUCKET when that env var is:
+# Defined AND contains the value true
+if [[ -z "$S3BUCKET" || ! "$S3BUCKET" == "true" ]] ; then
+  cp -f -v files/space_cat.png /usr/share/html && \
+  cp -f -v files/hyttioaoa.png /usr/share/html
+  exit 0
+fi
+
+if [[ "$S3BUCKET" == "true" ]]; then
+  aws s3 sync s3://scw-initcont-exp01/exp01-annoydata/ /usr/share/html/
+  exit 0
+fi
+
+# if we got here, maybe we should "exit 1"
---
+++ deployment/create_secret.sh
@@ -0,0 +1,19 @@
+if [[ -z "$RO_AWS_ACCESS_KEY_ID" ]] ; then
+  echo "You must set the RO_AWS_ACCESS_KEY_ID environment variable"
+  exit 1
+fi
+
+if [[ -z "$RO_AWS_SECRET_ACCESS_KEY" ]] ; then
+  echo "You must set the RO_AWS_SECRET_ACCESS_KEY environment variable"
+  exit 1
+fi
+
+if [[ -z "$RO_AWS_DEFAULT_REGION" ]] ; then
+  echo "You must set the RO_AWS_DEFAULT_REGION environment variable"
+  exit 1
+fi
+
+oc create secret generic initcont-aws-ro \
+--from-literal=AWS_ACCESS_KEY_ID=$RO_AWS_ACCESS_KEY_ID \
+--from-literal=AWS_SECRET_ACCESS_KEY=$RO_AWS_SECRET_ACCESS_KEY \
+--from-literal=AWS_DEFAULT_REGION=$RO_AWS_DEFAULT_REGION
