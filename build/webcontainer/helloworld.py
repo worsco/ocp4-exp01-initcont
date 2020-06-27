5bd098003d8d146a6dc6b33fc6b792b0dc994bcb
Scott Worthington <scworthi@redhat.com>
2020-06-27 14:41:48 -0400

    disable browser caching

--- build/webcontainer/helloworld.py
+++ build/webcontainer/helloworld.py
@@ -11,9 +11,22 @@ app = Flask(__name__,
             static_folder=MYDATADIR,
             template_folder=MYTEMPLATESDIR)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/')
 def main():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     print("Hello World! Built with a Dockerfile.")
